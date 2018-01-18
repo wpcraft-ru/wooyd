411191865e9b4721deb874784d889a1f19bdc07f
yumashev <yumashev@fleep.io>
2018-01-18 22:28:56 +0300

    add settings

--- inc/class-settings.php
+++ inc/class-settings.php
@@ -22,11 +22,12 @@ class WooYD_Settings
 
   }
 
+
   function settings_general(){
     add_settings_section(
-      'wooid_general',
+      'wooyd_general',
       'Основные настройки',
-      null,
+      $function = array($this, 'display_section_general'),
       'yandex_delivery_wpc'
     );
 
@@ -36,33 +37,79 @@ class WooYD_Settings
       $title = 'Корзинный виджет',
       $callback = [$this, 'display_wooid_script_cart_widget'],
       $page = 'yandex_delivery_wpc',
-      $section = 'wooid_general'
+      $section = 'wooyd_general'
     );
 
     register_setting('yandex_delivery_wpc', 'wooid_script_track_widget');
     add_settings_field(
-      $id = 'wooid_script_track_widget',
+      $id = 'wooid_script_track_widget', //@TODO: fix for wooyd
       $title = 'Трекинг-виджет',
       $callback = [$this, 'display_wooid_script_track_widget'],
       $page = 'yandex_delivery_wpc',
-      $section = 'wooid_general'
+      $section = 'wooyd_general'
+    );
+
+    register_setting('yandex_delivery_wpc', 'wooyd_widget_cart');
+    add_settings_field(
+      $id = 'wooyd_widget_cart',
+      $title = 'Карточный виджет',
+      $callback = [$this, 'display_wooyd_widget_cart'],
+      $page = 'yandex_delivery_wpc',
+      $section = 'wooyd_general'
+    );
+
+    register_setting('yandex_delivery_wpc', 'wooyd_widget_info');
+    add_settings_field(
+      $id = 'wooyd_widget_info',
+      $title = 'Инфо-виджет',
+      $callback = [$this, 'display_wooyd_widget_info'],
+      $page = 'yandex_delivery_wpc',
+      $section = 'wooyd_general'
+    );
+
+    register_setting('yandex_delivery_wpc', 'wooyd_widget_geo');
+    add_settings_field(
+      $id = 'wooyd_widget_geo',
+      $title = 'Гео-виджет',
+      $callback = [$this, 'display_wooyd_widget_geo'],
+      $page = 'yandex_delivery_wpc',
+      $section = 'wooyd_general'
     );
 
   }
 
+  //Display instruction for general section
+  function display_section_general(){
+    ?>
+    <p>Настройки нужно скопировать из панели управления Яндекс Доставка: <a href="https://delivery.yandex.ru/integration/index" target="_blank">https://delivery.yandex.ru/integration/index</a></p>
+    <?php
+  }
+
   function display_wooid_script_cart_widget(){
     $name = 'wooid_script_cart_widget';
-    printf('<textarea rows="5" cols="95" name="%s">%s</textarea>',$name, get_option($name));
+    printf('<textarea rows="3" cols="95" name="%s">%s</textarea>',$name, get_option($name));
   }
   function display_wooid_script_track_widget(){
     $name = 'wooid_script_track_widget';
-    printf('<textarea rows="15" cols="95" name="%s">%s</textarea>',$name, get_option($name));
+    printf('<textarea rows="10" cols="95" name="%s">%s</textarea>',$name, get_option($name));
+  }
+  function display_wooyd_widget_cart(){
+    $name = 'wooyd_widget_cart';
+    printf('<textarea rows="10" cols="95" name="%s">%s</textarea>',$name, get_option($name));
+  }
+  function display_wooyd_widget_info(){
+    $name = 'wooyd_widget_info';
+    printf('<textarea rows="10" cols="95" name="%s">%s</textarea>',$name, get_option($name));
+  }
+  function display_wooyd_widget_geo(){
+    $name = 'wooyd_widget_geo';
+    printf('<textarea rows="10" cols="95" name="%s">%s</textarea>',$name, get_option($name));
   }
 
   function settings_display(){
     // var_dump($option); exit;
     ?>
-    <h1>Настройки Яндекс Доставка</h1>
+    <h1>Настройки интеграции для "Яндекс Доставка"</h1>
     <form method="POST" action="options.php">
       <?php
         settings_fields( 'yandex_delivery_wpc' );
